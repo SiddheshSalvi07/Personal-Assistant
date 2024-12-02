8f5d3c9f94e2e3abd8d24af36b964b8ef9faa5de
Siddhesh <salvisiddhesh453@gmail.com>
2024-12-02 23:23:08 +0530

    SpeechRecognization module Done

--- engine/command.py
+++ engine/command.py
@@ -1,11 +1,36 @@
 import pyttsx3
+import speech_recognition as sr
 
 def Speak(text):
     engine=pyttsx3.init('sapi5')
     voices=engine.getProperty('voices')
     engine.setProperty('voice',voices[0].id)
-    engine.setProperty('rate',174)
+    engine.setProperty('rate',150)
     print(voices)
     engine.say(text)
     engine.runAndWait()
-Speak("i love india")
+
+def takecommand():
+    r=sr.Recognizer()
+
+    with sr.Microphone() as source:
+        print("listening......")
+        r.pause_threshold = 1
+        r.adjust_for_ambient_noise(source)
+
+        audio=r.listen(source, 10, 10)
+
+    try:
+        print("recognizing....")
+        query=r.recognize_google(audio, language="en-in")
+        print(f"User said: {query}")
+    except Exception as e:
+        return ""
+    
+    return query.lower()
+
+
+text=takecommand()
+
+Speak(text)
+
